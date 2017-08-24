3e4f48e4a3b6fee10c217ecea2bb2a70afb3b184
jyri <Jyri Soppela>
2017-08-24 13:56:26 +0300

    handle python version in a smarter way

--- deploy_lambda.py
+++ deploy_lambda.py
@@ -1,22 +1,23 @@
 #import boto3
 import os
 import argparse
+import sys
 
 def upload_deployment_package(function, package):
     print("We got this far!")
 
 
-def make_deployment_package(function, python_version):
+def make_deployment_package(lambda_function, python_version):
     cwd = os.getcwd()
-    packages = os.listdir('{0}/{1}_env/lib/{2}/site-packages'.format(cwd, function, python_version))
-    os.system('cp -r {0}/{1}_env/lib/{2}/site-packages/* {0}/{1}'.format(cwd, function, python_version))
+    packages = os.listdir('{0}/{1}_env/lib/{2}/site-packages'.format(cwd, lambda_function, python_version))
+    os.system('cp -r {0}/{1}_env/lib/{2}/site-packages/* {0}/{1}'.format(cwd, lambda_function, python_version))
     os.system('mkdir -p {0}/lambda_packages'.format(cwd))
-    os.system('rm -f {0}/lambda_packages/{1}.zip'.format(cwd, function))
-    os.system('zip -q -r {0}/lambda_packages/{1}.zip {0}/{1}'.format(cwd, function))
+    os.system('rm -f {0}/lambda_packages/{1}.zip'.format(cwd, lambda_function))
+    os.system('zip -q -r {0}/lambda_packages/{1}.zip {0}/{1}'.format(cwd, lambda_function))
 
     # cleanup copied packages
     for pkg in packages:
-        os.system('rm -r {0}/{1}/{2}'.format(cwd, function, pkg))
+        os.system('rm -r {0}/{1}/{2}'.format(cwd, lambda_function, pkg))
     return 'foo'
 
 if __name__ == "__main__":
@@ -24,13 +25,13 @@ if __name__ == "__main__":
     parser.add_argument('function', type=str, help='name of Lambda function to deploy')
     parser.add_argument('-c', type=str, help='name of the country to deploy the functions to', default='demo')
     #parser.add_argument('-d', '--dependencies', type=str, help='path to dependencies directory', default='')
-    parser.add_argument('-p', '--python_version', type=str, help='python interpreter version to use',
-                        default='python35')
+    parser.add_argument('-p', '--python_interpreter', type=str, help='python interpreter version to use',
+                        default='/usr/bin/python')
 
     args = parser.parse_args()
-    print(vars(args))
-    function = vars(args)['function']
-    package = make_deployment_package(function=function,
-                                      python_version=vars(args)['python_version'])
-    upload_deployment_package(function=function,
+    lambda_function = vars(args)['function']
+    python_version = 'python{0}'.format(sys.version[:3])
+    package = make_deployment_package(lambda_function=lambda_function,
+                                      python_version=python_version)
+    upload_deployment_package(function=lambda_function,
                               package=package)
