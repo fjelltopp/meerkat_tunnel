270f8499d4921a4dcafaee0e743c14c31bf1108a
jyri <Jyri Soppela>
2017-08-24 15:43:47 +0300

    more zip fixing? why not

--- deploy_lambda.py
+++ deploy_lambda.py
@@ -31,11 +31,18 @@ def make_deployment_package(lambda_function, python_version):
     os.system('cp -r {0}/{1}_env/lib/{2}/site-packages/* {0}/{1}'.format(cwd, lambda_function, python_version))
     os.system('mkdir -p {0}/lambda_packages'.format(cwd))
     os.system('rm -f {0}/lambda_packages/{1}.zip'.format(cwd, lambda_function))
-    os.system('zip -q -r {0}/lambda_packages/{1}.zip ./{1}/*'.format(cwd, lambda_function))
+
+    # change directory to zip on correct folder level
+    os.chdir('{0}/{1}'.format(cwd, lambda_function))
+    os.system('zip -q {0}/lambda_packages/{1}.zip {1}.py'.format(cwd, lambda_function))
 
     # cleanup copied packages
     for pkg in packages:
+        os.system('zip -q {0}/lambda_packages/{1}.zip {2}'.format(cwd, lambda_function, pkg))
         os.system('rm -r {0}/{1}/{2}'.format(cwd, lambda_function, pkg))
+
+    # go back to original working directory
+    os.chdir(cwd)
     print('Deployment package {0}.zip created'.format(lambda_function))
     return '{0}/lambda_packages/{1}.zip'.format(cwd, lambda_function)
 
