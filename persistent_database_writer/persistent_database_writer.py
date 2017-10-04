2c175188cda968018f71727f5c788a00e8c87647
jyri <Jyri Soppela>
2017-10-04 12:08:42 +0300

    new queue naming convention for database writer

--- persistent_database_writer/persistent_database_writer.py
+++ persistent_database_writer/persistent_database_writer.py
@@ -38,8 +38,8 @@ class PersistentDatabaseWriter:
         :param subscription_arn: Subscription ARN
         :return: returns 1 to 10 messages from SQS
         """
-        endpoint = subscription_arn.split(':')
-        return incoming_queue + '-' + endpoint[-1]
+        #endpoint = subscription_arn.split(':')
+        return incoming_queue # + '-' + endpoint[-1]
 
     def get_queue_url(self, queue_name):
         """
