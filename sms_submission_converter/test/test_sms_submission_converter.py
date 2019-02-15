368cee044fc58958665559072eb6704344a5b04a
jyri <soppela.jyri@gmail.com>
2019-02-15 14:00:29 +0200

    unit test framework

---
+++ sms_submission_converter/test/test_sms_submission_converter.py
@@ -0,0 +1,24 @@
+"""
+Lambda SMS Converter Test
+"""
+
+import unittest
+from unittest.mock import MagicMock, call
+import datetime
+from dateutil.tz import tzutc
+
+import sms_submission_converter.sms_submission_converter
+
+
+class SmsSubmissionConverterTest(unittest.TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        """Setup for testing"""
+
+    def setUp(self):
+        self.event = {
+            'queue': 'nest-test-queue',
+            'dead-letter-queue': 'nest-test-dead-letter-queue'
+        }
+        self.converter = sms_submission_converter.sms_submission_converter.NestQueueConsumer())
