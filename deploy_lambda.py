1a928ad414a44e5b0fbac41ed21862c1c37b3f0e
Tomasz Sabała <sabala.tomek@gmail.com>
2019-03-01 12:49:57 +0100

    Updating the dir structure of the project. Simplifying deploy_lambda filesystem logic.

--- deploy_lambda.py
+++ deploy_lambda.py
@@ -60,33 +60,28 @@ def make_deployment_package(lambda_function, python_version):
     # copy packages
     cwd = os.getcwd()
 
-    if os.path.isdir('{0}/{1}_env/lib/{2}/site-packages'.format(cwd, lambda_function, python_version)):
-        packages = os.listdir('{0}/{1}_env/lib/{2}/site-packages'.format(cwd, lambda_function, python_version))
-        os.system('cp -r {0}/{1}_env/lib/{2}/site-packages/* {0}/{1}'.format(cwd, lambda_function, python_version))
+    site_packages_lib_format = '{0}/venvs/{1}_env/lib/{2}/site-packages'.format(cwd, lambda_function, python_version)
+    site_packages_lib64_format = '{0}/venvs/{1}_env/lib64/{2}/site-packages'.format(cwd, lambda_function, python_version)
+    if os.path.isdir(site_packages_lib_format):
+        packages = os.listdir(site_packages_lib_format)
     else:
         packages = []
-    if os.path.isdir('{0}/{1}_env/lib64/{2}/site-packages'.format(cwd, lambda_function, python_version)):
-        packages64 = os.listdir('{0}/{1}_env/lib64/{2}/site-packages'.format(cwd, lambda_function, python_version))
-        os.system('cp -r {0}/{1}_env/lib64/{2}/site-packages/* {0}/{1}'.format(cwd, lambda_function, python_version))
+    if os.path.isdir(site_packages_lib64_format):
+        packages64 = os.listdir(site_packages_lib64_format)
     else:
         packages64 = []
     os.system('mkdir -p {0}/lambda_packages'.format(cwd))
     os.system('rm -f {0}/lambda_packages/{1}.zip'.format(cwd, lambda_function))
 
-    # change directory to zip on correct folder level
-    os.chdir('{0}/{1}'.format(cwd, lambda_function))
-    os.system('zip -q {0}/lambda_packages/{1}.zip {1}.py'.format(cwd, lambda_function))
+    os.system('zip -q {0}/lambda_packages/{1}.zip lambdas/{1}.py'.format(cwd, lambda_function))
 
     for pkg in packages:
-        os.system('zip -q -r {0}/lambda_packages/{1}.zip {2}'.format(cwd, lambda_function, pkg))
-        os.system('rm -r {0}/{1}/{2}'.format(cwd, lambda_function, pkg))
+        pkg_path = os.path.join(site_packages_lib_format, pkg)
+        os.system('zip -q -r {0}/lambda_packages/{1}.zip {2}'.format(cwd, lambda_function, pkg_path))
 
     for pkg64 in packages64:
-        os.system('zip -q -r {0}/lambda_packages/{1}.zip {2}'.format(cwd, lambda_function, pkg64))
-        os.system('rm -r {0}/{1}/{2}'.format(cwd, lambda_function, pkg64))
-
-    # go back to original working directory
-    os.chdir(cwd)
+        pkg64_path = os.path.join(site_packages_lib_format, pkg64)
+        os.system('zip -q -r {0}/lambda_packages/{1}.zip {2}'.format(cwd, lambda_function, pkg64_path))
 
     print('Deployment package {0}.zip created'.format(lambda_function))
     return '{0}/lambda_packages/{1}.zip'.format(cwd, lambda_function)
