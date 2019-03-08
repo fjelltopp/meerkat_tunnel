987d8f333606eaa16a8767e4d4ef547c5a25d543
Tomasz Sabała <sabala.tomek@gmail.com>
2019-03-08 09:54:55 +0100

    Working prototype for parsing SMS submissions.

--- lambdas/sms_submission_converter.py
+++ lambdas/sms_submission_converter.py
@@ -1,8 +1,11 @@
 import collections
 import json
 import logging
-import requests
 import os
+import time
+
+import boto3
+import requests
 import xmltodict
 
 logger = logging.getLogger()
@@ -11,14 +14,70 @@ logger.setLevel(logging.INFO)
 
 class SmsSubmissionConverter:
     def __init__(self):
-        region_name = 'eu-west-1'
-
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.INFO)
 
-    def prepare_payload(self, payload):
-        t = json.loads(payload['body'].replace("'", '"'))
-        submission = t['text'].split(";")
+    def store_multi_sms_payload(self, payload):
+        body = json.loads(payload['body'])
+        ref = body['concat-ref']
+        total = body['concat-total']
+        part = body['concat-part']
+        text = body['text']
+        client = boto3.client('dynamodb')
+        client.put_item(
+            Item={
+                'ref': {
+                    'S': ref,
+                },
+                'total': {
+                    'N': total,
+                },
+                'part': {
+                    'S': part,
+                },
+                'text': {
+                    'S': text,
+                },
+                'receivedTime': {
+                    'S': str(time.strftime("%Y-%m-%d %H:%M %Z"))
+                }
+            },
+            ReturnConsumedCapacity='TOTAL',
+            TableName='SmsParts',
+        )
+        r = client.update_item(
+            TableName='SmsReceived',
+            Key={
+                'ref': {
+                    'S': ref
+                }
+            },
+            ExpressionAttributeNames={
+                '#count': "count"
+            },
+            UpdateExpression='ADD #count :inc',
+            ExpressionAttributeValues={
+                ':inc': {'N': '1'}
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        received_count = int(r['Attributes']['count']['N'])
+        is_complete = received_count == int(total)
+        logger.info(f"Counter: received {received_count}, total {total}, is_complete {is_complete}")
+        return ref, total, part, is_complete
+
+    def is_multi_sms_payload(self, payload):
+        logger.info(f"Got2: {payload}")
+        body = json.loads(payload['body'])
+        return body.get('concat', '').lower() == 'true'
+
+    def prepare_single_sms_payload(self, payload):
+        body = json.loads(payload['body'])
+        return body['text']
+
+    def format_payload(self, text):
+        logger.info(f"Got: {text}")
+        submission = text.split(";")
 
         if submission[-1] == '':
             submission.pop(-1)
@@ -47,9 +106,9 @@ class SmsSubmissionConverter:
         return form_definition
 
     def payload_json_to_xml(self, form_definition, prepared_payload):
-        ids_map = self._short_to_long_field_names(form_definition)
-        submission_dict = self._submission_to_ordered_dict(ids_map, prepared_payload)
         form_id = prepared_payload['form_id']
+        ids_map = self._short_to_long_field_names(form_definition, form_id)
+        submission_dict = self._submission_to_ordered_dict(ids_map, prepared_payload)
         xml_str = self._to_xml_string(form_id, submission_dict)
         return xml_str
 
@@ -75,11 +134,10 @@ class SmsSubmissionConverter:
                 submission_dict[field_name] = value
         return submission_dict
 
-    def _short_to_long_field_names(self, form_definition):
-        form_fields = form_definition['h:html']['h:head']['model']['instance']['sms_test_form']
+    def _short_to_long_field_names(self, form_definition, form_id):
+        form_fields = form_definition['h:html']['h:head']['model']['instance'][0][form_id]
         ids_map = dict()
         for key, field in form_fields.items():
-            print(key)
             try:
                 short_id = field['@odk:tag']
                 ids_map[short_id] = {
@@ -103,21 +161,46 @@ class SmsSubmissionConverter:
         return ids_map
 
 
+    def get_complete_multi_sms_payload(self, ref):
+        client = boto3.client('dynamodb')
+        response = client.query(TableName='SmsParts',
+                                ExpressionAttributeValues={':ref_value': {'S': str(ref)}},
+                                ExpressionAttributeNames={'#ref_alias': 'ref'},
+                                KeyConditionExpression='#ref_alias = :ref_value'
+                                )
+        return ''.join([part['text']['S'] for part in response['Items']])
+
+
 def lambda_handler(event, context):
-    #logger.info('got event{}'.format(event))
     translator = SmsSubmissionConverter()
+    is_multi_sms = translator.is_multi_sms_payload(event)
+    if is_multi_sms:
+        logger.info("Multi part sms submission.")
+        ref, total, part, is_complete = translator.store_multi_sms_payload(event)
+        if is_complete:
+            logger.info("Multi part sms submission is complete.")
+            raw_payload = translator.get_complete_multi_sms_payload(ref)
+        else:
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Hello from Lambda!')
+            }
+    else:
+        logger.info("Single sms submission.")
+        raw_payload = translator.prepare_single_sms_payload(event)
 
-    payload = translator.prepare_payload(event)
-
+    payload = translator.format_payload(raw_payload)
     aggregate_url = os.environ.get('AGGREGATE_URL')
 
     form_definition = translator.get_form_definition(aggregate_url, payload['form_id'])
+    submission_xml = translator.payload_json_to_xml(form_definition, payload)
 
-    logger.info('form xml {}'.format(str(form_definition)))
-    logger.info(f'payload: {payload}')
+    files = {'xml_submission_file': ('form.xml', submission_xml, "text/xml")}
+    r = requests.post(aggregate_url + "/submission", files=files)
+
+    logger.info(f'Send submission to aggregate with code {r.status_code}')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-
 
